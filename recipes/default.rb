d4da1aae5c2ad90cf0ffe048704fbbc7db24cd3e
Julian C. Dunn <jdunn@chef.io>
2015-09-23 18:31:59 -0400

    Add some comments

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,18 +17,23 @@
 # limitations under the License.
 #
 
+# You can put any Ruby in the block.
 Chef.event_handler do
   on :converge_start do
     puts "Ohai! I have started a converge."
   end
 end
 
+# You can also invoke methods inside libraries.
+# Just remember that Chef.run_context may not be fully populated based
+# on the type of event you're trying to hook into.
 Chef.event_handler do
   on :run_failed do
     HandlerDemo::Helper.new.send_email_on_failure(Chef.run_context.node.name)
   end
 end
 
+# Let's cause an exception so that we get an email.
 ruby_block 'fail the run to show off the foregoing' do
   block do
     fail 'deliberately failing the run'

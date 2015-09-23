141c6d2f6358f73473caeedbe8fbdc295000b720
Julian C. Dunn <jdunn@chef.io>
2015-09-23 18:47:12 -0400

    Use double quotes so interpolation actually works

--- libraries/helper.rb
+++ libraries/helper.rb
@@ -5,11 +5,11 @@ module HandlerDemo
 
     def send_email_on_failure(node_name)
 
-      message  = 'From: Chef <chef@chef.io>\n'
-      message << 'To: Julian <jdunn@chef.io>\n'
-      message << 'Subject: chef-client Run Failed\n'
-      message << 'Date: #{Time.now.rfc2822}\n\n'
-      message << 'Chef run failed on #{node.name}\n'
+      message = "From: Chef <chef@chef.io>\n"
+      message << "To: Julian <jdunn@chef.io>\n"
+      message << "Subject: chef-client Run Failed\n"
+      message << "Date: #{Time.now.rfc2822}\n\n"
+      message << "Chef run failed on #{node.name}\n"
       Net::SMTP.start('localhost', 25) do |smtp|
         smtp.send_message message, 'chef@chef.io', 'jdunn@chef.io'
       end    
